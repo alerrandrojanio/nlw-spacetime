ae479b1be757fbc3e26aebef3a8b15cd2113ad73
Alerrandro Janio <alerrandro.janio07@gmail.com>
2023-05-15 22:03:31 -0300

    config ambientes back, web e mobile

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import fastify from "fastify"
+
+const app = fastify()
+const port = 3333
+
+app
+  .listen({
+    port,
+  })
+  .then(() => {
+    console.log(`🚀 Server is running on port ${port}`)
+  })
