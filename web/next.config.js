18eff35c9aad6234beef2323168e0dd369ef29fe
alerrandro <alerrandro.janio07@gmail.com>
2023-05-23 08:43:45 -0300

    web: deploy test 2

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "192.168.0.16"],
+    domains: [
+      "avatars.githubusercontent.com",
+      "192.168.0.16",
+      "nlw-spacetime-41tahmtvo-alerrandrojanio.vercel.app:3333",
+      "nlw-spacetime-41tahmtvo-alerrandrojanio.vercel.app",
+    ],
   },
 };
 
